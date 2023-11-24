972ee16a981bb155431a9088f019c8816cd83c78
Pranmay <pranmayj09@gmail.com>
2023-11-24 16:42:09 +0530

    OOPs1

--- src/Threads/PrintNumbers/Client.java
+++ src/Threads/PrintNumbers/Client.java
@@ -1,11 +1,16 @@
 package Threads.PrintNumbers;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Client {
     public static void main(String[] args) {
+        ExecutorService ex = Executors.newFixedThreadPool(10); // only 10 thread will be used for all the tasks
         for (int i = 1; i <= 100; i++) {
             PrintNumbers x = new PrintNumbers(i);
-            Thread t = new Thread(x);
-            t.start();
+//            Thread t = new Thread(x); //for each separate thread for each task
+//            t.start();
+            ex.execute(x);
         }
     }
 }
