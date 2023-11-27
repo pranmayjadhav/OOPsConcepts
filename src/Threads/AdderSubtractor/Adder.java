3e9363c6937ff03bd472f776ce0699c0ce34a9d4
Pranmay <pranmayj09@gmail.com>
2023-11-27 23:16:18 +0530

    OOPs1

--- src/Threads/AdderSubtractor/Adder.java
+++ src/Threads/AdderSubtractor/Adder.java
@@ -1,4 +1,21 @@
 package Threads.AdderSubtractor;
 
-public class Adder {
+import java.util.concurrent.Callable;
+
+public class Adder implements Callable<Void> {
+    private Value v;
+    public Adder(Value x) {
+        this.v = x;
+    }
+
+    @Override
+    public Void call() throws Exception {
+        for(int i = 1; i <= 1000; i++) {
+            this.v.value += i;
+        }
+        return null;
+    }
+
+
+
 }
---
+++ src/Threads/AdderSubtractor/Value.java
@@ -0,0 +1,5 @@
+package Threads.AdderSubtractor;
+
+public class Value {
+    public int value = 0;
+}
